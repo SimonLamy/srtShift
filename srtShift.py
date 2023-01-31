0112b44d4a1ad7f0b4ca8a4c0a179f0974278b0f
SimonLamy <34168259+SimonLamy@users.noreply.github.com>
2023-01-31 21:43:59 +0100

    Update srtShift.py

--- srtShift.py
+++ srtShift.py
@@ -16,9 +16,9 @@ os.chdir(os.path.dirname(input))
 if argLen != 4 :
     print("Error : Too much or missing arguments.")
 elif input.endswith('.srt')== False :
-    print('Wrong input file type. Need .srt file. )
+    print("Wrong input file type. Need .srt file." )
 elif input.endswith('.srt')== False :
-    print('Wrong output file type. Name the generated file with .srt extension. ')
+    print("Wrong output file type. Name the generated file with .srt extension.")
 else :
     for sub in range(0,subsLen) :
         subs[sub].start = subs[sub].start + datetime.timedelta(seconds = delay)
